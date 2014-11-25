ff829be6cd709557d0e14ea4cbae1b7cd78dacd8
thmain <tutorialhorizon@gmail.com>
2014-11-24 23:41:42 -0800

    Streamlined the grunt file and removes excess npm packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,15 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   grunt.initConfig({});
 
-  grunt.config( 'webpack', require('./grunt/webpack.js') );
   grunt.config( 'react', require('./grunt/react.js') );
   grunt.config( 'watch', require('./grunt/watch.js') );
 
-  grunt.loadNpmTasks('grunt-webpack');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-react');
 
-  grunt.registerTask('default', ['react', 'webpack']);
+  grunt.registerTask('dist', ['react:dist']);
+  grunt.registerTask('examples', ['react:examples']);
+  grunt.registerTask('dev', ['watch']);
 
 };
