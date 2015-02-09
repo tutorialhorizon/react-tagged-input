206b99c6d7f169d33c4cd1026928e2b0f96a7da2
Matthew Doyle <matthew.doyle@envato.com>
2015-02-09 23:17:07 +1100

    onBeforeRemoveTag expects an index as an argument

--- src/TaggedInput.jsx
+++ src/TaggedInput.jsx
@@ -57,7 +57,7 @@ var TaggedInput = React.createClass({
       onBeforeAddTag: function (tag) {
         return true;
       },
-      onBeforeRemoveTag: function (tag) {
+      onBeforeRemoveTag: function (index) {
         return true;
       }
     };
