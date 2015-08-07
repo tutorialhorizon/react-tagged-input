e4c30923a73e49d7c50a0918feebafd76f0d9c5c
thmain <tutorialhorizon@gmail.com>
2015-08-06 22:15:22 -0700

    Added jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,15 @@ module.exports = function (grunt) {
 
   grunt.initConfig({});
 
-  grunt.config('webpack', require('./grunt/webpack.js'));
   grunt.loadNpmTasks('grunt-webpack');
-  grunt.registerTask('dev', ['webpack']);
-
-  grunt.config('react', require('./grunt/react.js'));
+  grunt.loadNpmTasks('grunt-jsxhint-babel');
   grunt.loadNpmTasks('grunt-react');
+
+  grunt.config('webpack', require('./grunt/webpack.js'));
+  grunt.config('react', require('./grunt/react.js'));
+  grunt.config('jshint', require('./grunt/jshint.js'));
+
+  grunt.registerTask('dev', ['webpack']);
   grunt.registerTask('build', ['react']);
 
 };
